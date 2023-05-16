a2cc1e1ea92d1b94eb8ad53536d6938d69a55150
alaz0221 <aleksandr.azarenok@netcracker.com>
2023-05-16 14:01:03 +0400

    [23]

--- backend/src/resolvers/note.js
+++ backend/src/resolvers/note.js
@@ -6,9 +6,9 @@ module.exports = {
     favoritedBy: async (note, args, { models }) => {
       return models.User.find({ _id: { $in: note.favoritedBy } });
     },
-    comments: async (note, args, { models }) => {
-      const author = await models.User.findById(note.author);
-      return note.comments.map((comment) => {
+    comments: (note, args, { models }) => {
+      return note.comments.map(async (comment) => {
+        const author = await models.User.findById(comment.author);
         return {
           id: comment._id,
           text: comment.text,
